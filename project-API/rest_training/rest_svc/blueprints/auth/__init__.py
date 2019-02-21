4c98f1f2b5dcc85b150f75903193cd0cf7c1560b
fatmajanna <fatma.janna.utp@gmail.com>
2019-02-21 20:40:54 +0700

    Done with creating users: admin and public and creating token

--- project-API/rest_training/rest_svc/blueprints/auth/__init__.py
+++ project-API/rest_training/rest_svc/blueprints/auth/__init__.py
@@ -2,7 +2,7 @@ import logging, json
 from flask import Blueprint
 from flask_restful import Api, Resource, reqparse, marshal
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, get_jwt_claims
-from blueprints.client import *
+from blueprints.user import *
 
 bp_auth = Blueprint('auth', __name__)
 api = Api(bp_auth)
@@ -10,12 +10,16 @@ api = Api(bp_auth)
 class CreateTokenResources(Resource):
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('client_key', location='json', required=True)
-        parser.add_argument('client_secret', location='json', required=True)
+        parser.add_argument('user_type', location='args', required=True)
+        parser.add_argument('name', location='json', required=True)
+        parser.add_argument('password', location='json', required=True)
+        
         args = parser.parse_args()
-        qry = Clients.query.filter_by(client_key=args['client_key']).filter_by(client_secret=args['client_secret']).first()
+        if args['user_type'] == None:
+            args['user_type'] = 'publik'
+        qry = Users.query.filter_by(user_type=args['user_type']).filter_by(name=args['name']).filter_by(password=args['password']).first()
         if qry is not None:    
-            token = create_access_token(identity=marshal(qry, Clients.response_field))
+            token = create_access_token(identity=marshal(qry, Users.response_field))
             return {'token': token}, 200
         else:
             return {'status': 'UNAUTHORIZED', 'message': 'invalid key or secret'}, 401
